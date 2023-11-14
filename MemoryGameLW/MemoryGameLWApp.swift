0ee547b657778ec0e173f32e10fd8dbc84c8c8bb
lukasz wazelin <lukasz.wazelin@pollub.edu.pl>
2023-11-14 16:25:15 +0100

    Initial Commit

---
+++ MemoryGameLW/MemoryGameLWApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameLWApp.swift
+//  MemoryGameLW
+//
+//  Created by student on 14/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameLWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
